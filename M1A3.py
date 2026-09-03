1e7e28eb8c875a1ef462fb2b1df983bb33b2a237
abarryV04 <abarry@csu.fullerton.edu>
2026-09-03 08:57:51 -0700

    Update M1A3.py

--- M1A3.py
+++ M1A3.py
@@ -1,4 +1,12 @@
 #Name: Aidan Barry
 #Student ID: 873622484
-#Section: 18254
+#Section: 07
 #Assignment: Module 1 Assignment 3
+
+first_int = int(input("What is the integer? "))
+second_float = float(input("What is the float? "))
+
+print(f"{first_int} + {second_float} is {first_int + second_float}")
+print(f"{first_int} - {second_float} is {first_int - second_float}")
+print(f"{first_int} * {second_float} is {first_int * second_float}")
+print(f"{first_int} / {second_float} is {first_int / second_float}")
