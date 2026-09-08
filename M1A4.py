d848ab8ca46da216350ce644a4411708e3535c41
abarryV04 <abarry@csu.fullerton.edu>
2026-09-08 08:17:18 -0700

    Update M1A4.py

--- M1A4.py
+++ M1A4.py
@@ -1,7 +1,7 @@
-#Name: Aidan Barry
-#Student ID: 873622484
-#Section: 07
-#Assignment: Module 1 Assignment 4
+# Name: Aidan Barry
+# Student ID: 873622484
+# Section: 07
+# Assignment: Module 1 Assignment 4
 
 catchphrase_str = input("Please enter a catchphrase: ")
 print(f"[{catchphrase_str.upper()}]")
